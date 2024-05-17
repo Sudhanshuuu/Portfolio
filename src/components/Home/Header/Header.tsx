125f1602c99fbe2e66c3144a3df75db4191fc16a
Sudhanshuuu <sudhanshusingh9754@gmail.com>
2024-05-17 17:52:19 +0530

    first commit

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
             <div className=" w-[80%] flex justify-center flex-col md:w-[50%]">
                 <div>WEB DEVELOPER</div>
                 <div className=" text-6xl font-bold my-[3%]">Hello, my name is <br /><span className="text-5xl">Sudhanshu Singh</span> </div>
-                <div className="my-[3%]">I wasn't a code lover before college; my journey into coding began the moment I entered college. I wasn't sure what I wanted to do or how to do it. But as soon as I started the learning process, I fell in love with it. The process of learning, creating, and getting better at something thrives me.</div>
+                <div className="my-[3%]">I was not a code lover before college, my journey into coding began the moment I entered college. I was not sure what I wanted to do or how to do it. But as soon as I started the learning process, I fell in love with it. The process of learning, creating, and getting better at something thrives me.</div>
                 <div className="flex">
                     <button className="mx-2 text-sm border border-black px-4 py-3 rounded-3xl transition duration-200 hover:bg-black hover:text-white sm:text-base" onClick={handleClick}>Contact Me</button>
                     <button className="mx-2 text-sm bg-black text-white px-4 py-3 rounded-3xl border border-black transition duration-200 hover:bg-white hover:text-black">
